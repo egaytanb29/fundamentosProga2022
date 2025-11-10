23a344fd053f16ab03a9915231826486413cf4a4
Ernesto Gaytán <123116609+egaytanb29@users.noreply.github.com>
2025-11-10 15:52:55 -0600

    Create calificacion.c

---
+++ calificacion.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void calificar(void);
+
+int main (void)
+{
+        calificar();
+
+        return 0;
+}
+
+void calificar (void)
+{
+        int calificacion;
+
+        printf("Escribe tu calificación: ");
+        scanf("%d",&calificacion);
+
+        if (calificacion >= 6) {
+                printf("Pasaste!\n");
+        } //Fin if//
+
+        else {
+                printf("Reprobado\n");
+        } //Fin else//
+
+}
