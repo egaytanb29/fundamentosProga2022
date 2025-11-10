0c8689a131e05a7eac9181fa1d4ae4e6b59347cd
Ernesto Gaytán <123116609+egaytanb29@users.noreply.github.com>
2025-11-10 14:53:21 -0600

    Create holaMundo.c

---
+++ holaMundo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void saluda(void);
+
+int main(void)
+{
+        saluda();
+        return 0;
+}
+
+void saluda(void)
+{
+        printf("Hola mundo\n");
+        printf("Adios mundo\n");
+}
